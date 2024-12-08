6f41c2e34749dce4e09f05e4799edc49d9ba7f51
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2024-12-08 04:49:33 +0000

    defined class ListNode
    common class used for many problems involving linked lists

---
+++ common/ListNode.cpp
@@ -0,0 +1,14 @@
+/*
+    common definition of ListNode
+    used in problems:
+        21. Merge Two Sorted Lists
+*/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
