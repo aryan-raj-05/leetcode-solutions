da59c80ce8548027c0267364cd29cde85e806b72
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-02-07 20:57:54 +0530

    fix: bugs and other non essential solutions

--- neetcode/linked-list/0002-add.py
+++ neetcode/linked-list/0002-add.py
@@ -9,69 +9,6 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        i = l1
-        j = l2
-        res = k = ListNode()
-
-        carry = 0
-        while i and j:
-            total = i.val + j.val + carry
-
-            k.next = ListNode(total % 10)
-
-            carry = total // 10
-            k = k.next
-            i = i.next
-            j = j.next
-
-        while i:
-            total = i.val + carry
-
-            k.next = ListNode(total % 10)
-
-            carry = total // 10
-            k = k.next
-            i = i.next
-
-        while j:
-            total = j.val + carry
-
-            k.next = ListNode(total % 10)
-        
-            carry = total // 10
-            k = k.next
-            j = j.next
-
-        if carry > 0:
-            k.next = ListNode(carry)
-
-        return res.next
-    
-    def addTwoNumbers2(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        i1, i2 = l1, l2
-        carry = 0
-
-        dummyNode = k = ListNode()
-
-        while i1 or i2 or carry > 0:
-            val1 = i1.val if i1 else 0
-            val2 = i2.val if i2 else 0
-
-            total = val1 + val2 + carry
-
-            k.next = ListNode(total % 10)
-            carry = total // 10
-
-            i1 = i1.next if i1 else None
-            i2 = i2.next if i2 else None
-            k = k.next
-
-        return dummyNode.next
-    
-    def addTwoNumbers3(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1:
-            return l2
-        
         i1, i2 = l1, l2
         carry = 0
         k = l1
@@ -82,15 +19,15 @@ class Solution:
 
             total = val1 + val2 + carry
         
-            k.val = total % 10
+            k.val = total % 10 # pyright: ignore
             carry = total // 10
 
             i1 = i1.next if i1 else None
             i2 = i2.next if i2 else None
 
-            if not k.next:
+            if not k.next and (i1 or i2 or carry > 0): # pyright: ignore
                 k.next = ListNode()
             
-            k = k.next
+            k = k.next # pyright: ignore
 
         return l1
