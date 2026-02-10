7038fec9e58bc90e3090ecd69b417195ebafba46
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-02-10 20:20:06 +0530

    solved: 338 counting bits

--- neetcode/bit-manipulation/0338-count-bits.py
+++ neetcode/bit-manipulation/0338-count-bits.py
@@ -1,6 +1,24 @@
 from typing import List
 
 class Solution:
+    def countBits2(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+
+        res = [0, 1]
+        j = 1
+        for i in range(2, n + 1):
+            if (i & (i - 1)) == 0:
+                res.append(1)
+                j = 1
+                continue
+
+            res.append(1 + res[j])
+            j += 1
+
+        return res
+
+
     # naive solution
     # time complexity = O(n log n)
     # optimized solution uses dp for linear time complexity
@@ -11,12 +29,8 @@ class Solution:
                 k = k & (k - 1)
                 count += 1
             return count
-        
+
         res = []
         for i in range(n + 1):
             res.append(setBits(i))
         return res
-
-n = 24
-sol = Solution()
-print(sol.countBits(n))
