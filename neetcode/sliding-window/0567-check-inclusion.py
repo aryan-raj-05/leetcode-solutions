48d8f9918363b0a9a5ba458abbcf714839bf2040
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-02-12 11:38:13 +0530

    solved: 567 permutation in string

---
+++ neetcode/sliding-window/0567-check-inclusion.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n1 > n2:
+            return False
+        
+        s1Freq = [0] * 26
+        windowFreq = [0] * 26
+
+        def match():
+            for i in range(26):
+                if s1Freq[i] != windowFreq[i]:
+                    return False
+            return True
+
+        for i in range(n1):
+            s1Freq[ord(s1[i]) - ord('a')] += 1
+            windowFreq[ord(s2[i]) - ord('a')] += 1
+
+        i, j = 0, n1 - 1
+        while j < n2:
+            if match():
+                return True
+
+            i += 1; j += 1
+            windowFreq[ord(s2[i - 1]) - ord('a')] -= 1
+            if j < n2:
+                windowFreq[ord(s2[j]) - ord('a')] += 1
+
+        return False
+    
+    def same(self, s1, s2):
+        n1, n2 = len(s1), len(s2)
+
+        i, j = 0, n1 - 1
+        str_match = sorted(s1)
+
+        while j < n2:
+            if str_match == sorted(s2[i:j+1]):
+                return True
+            i += 1
+            j += 1
+
+        return False
