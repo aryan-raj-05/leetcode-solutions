cd694556842439c0429d73a797e3339389dcdcca
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-04-04 22:32:33 +0530

    neetcode: solved 322
    coin change

---
+++ neetcode/1d-dp/0322-coins.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [2**31] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            for c in coins:
+                if (i - c) >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+
+        return dp[amount] if dp[amount] != 2**31 else -1
